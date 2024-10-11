12d47618837077c74a45d7008c0d4226f6081c12
안가은 <gaeun1021@sookmyung.ac.kr>
2024-10-11 15:47:35 +0900

    [Feat] #14 - 카테고리 분류 함수 구현

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from dotenv import load_dotenv
-from openai import OpenAI
+import openai
 from fastapi import FastAPI
 from pydantic import BaseModel
 import os
+
+from category_classifier import classify_category
 
 # .env 파일에서 환경 변수 로드
 load_dotenv()
@@ -10,20 +12,30 @@ load_dotenv()
 app = FastAPI()
 
 # OpenAI API Key 설정
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+openai.api_key = os.getenv("OPENAI_API_KEY")
+model_engine = "gpt-4"
+
 
 # 요청 본문을 처리하기 위한 Pydantic 모델
-class MessageRequest(BaseModel):
+class GenerateMessageRequest(BaseModel):
     prompt: str
     model: str = "gpt-4o-mini"
     max_tokens: int = 100
 
+class ClassifyMessageRequest(BaseModel):
+    title: str
+    author: str
+    isbn: str
+    description: str
+    max_tokens: int = 100
+
+
 # POST 요청을 처리하는 엔드포인트
 @app.post("/generate/")
-async def generate_text(request: MessageRequest):
+async def generate_text(request: GenerateMessageRequest):
     try:
         # OpenAI API 호출
-        response = client.chat.completions.create(model=request.model,
+        response = openai.Completion.create(model=request.model,
         messages=[{"role": "user", "content": request.prompt}],
         max_tokens=request.max_tokens)
         # API 응답에서 텍스트 부분 추출
@@ -31,3 +43,18 @@ async def generate_text(request: MessageRequest):
         return {"generated_text": generated_text}
     except Exception as e:
         return {"error": str(e)}
+    
+# POST 요청을 처리하는 엔드포인트
+@app.post("/classify/")
+async def classify(request: ClassifyMessageRequest):
+    try:
+        # 카테고리 분류 함수 호출
+        category = classify_category(request.title, request.author, request.isbn, request.description, request.max_tokens)
+        return {"title": request.title, "category": category}
+
+    except Exception as e:
+        return {"error": str(e)}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
